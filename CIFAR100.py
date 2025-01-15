714760b0baf47cf6dfc1721365a435286f0b65ab
einck <einck0@outlook.com>
2025-01-15 20:54:16 +0800

    123

--- CIFAR100.py
+++ CIFAR100.py
@@ -1,8 +1,17 @@
-import random
+sp;&emsp;<b>注明：这个属性编辑器适用于所有<a href='http://www.seeprettyface.com'>seeprettyface.com</a>出品的定制人脸生成器(仅1024px版)和官方生成器。之前这部分属于购买生成器的赠品，现在供免费下载研究。</b><br /><br /><br />
 
-# 模拟下载进度信息
-print("Downloading dataset...")
-for i in range(1, 11):fgzdfvzcvzvdfvcv;
-    print(f"Progress: {i * 10}%")
-
-# 模拟最终精度nal_accuracy:.3f}")
+## 可编辑内容
+共有23个生成人脸的调整方向，分别如下：<br /><br />
+&emsp;&emsp;age.npy - 调整年龄<br />
+&emsp;&emsp;angle_horizontal.npy - 在左右方向上调整人脸角度<br />
+&emsp;&emsp;angle_vertical.npy - 在上下方向上调整人脸角度<br />
+&emsp;&emsp;beauty.npy - 调整颜值<br />
+&emsp;&emsp;camera_rotation.npy - 调整摄像机角度<br />
+&emsp;&emsp;emotion_angry.npy - 调整此项可增添/减弱一些生气的情绪（调整步幅建议缩小）<br />
+&emsp;&emsp;emotion_disgust.npy - 调整此项可增添/减弱一些厌恶的情绪（调整步幅建议缩小）<br />
+&emsp;&emsp;emotion_easy.npy - 调整此项可增添/减弱一些平静的情绪（调整步幅建议缩小）<br />
+&emsp;&emsp;emotion_fear.npy - 调整此项可增添/减弱一些害怕的情绪（调整步幅建议缩小）<br />
+&emsp;&emsp;emotion_happy.npy - 调整此项可增添/减弱一些开心的情绪（调整步幅建议缩小）<br />
+&emsp;&emsp;emotion_sad.npy - 调整此项可增添/减弱一些伤心的情绪（调整步幅建议缩小）<br />
+&emsp;&emsp;emotion_surprise.npy - 调整此项可增添/减弱一些惊讶的情绪（调整步幅建议缩小）<br />
+&emsp;&emsp;exposure.npy - 调整光照<br />
